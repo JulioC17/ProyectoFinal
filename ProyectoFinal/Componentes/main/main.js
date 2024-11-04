f61121ff6a891c33d766c9ede815d0d04b8d55c8
JulioC17 <159253136+JulioC17@users.noreply.github.com>
2024-11-04 17:34:11 +0100

    maquetando

--- ProyectoFinal/Componentes/main/main.js
+++ ProyectoFinal/Componentes/main/main.js
@@ -3,7 +3,7 @@ import './main.css'
 //RJSWAB9AEJ3MWXW4EF6UZAS2T
 //Hacemos el fetch a la api
 const fetchData = async () =>{
-    const data = await fetch ("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Valencia/?key=RJSWAB9AEJ3MWXW4EF6UZAS2T&unitGroup=metric&lang=es")
+    const data = await fetch ("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Valencia/?key=RJSWAB9AEJ3MWXW4EF6UZAS2T&unitGroup=metric&lang=es&iconSetvalues=icons1")
     const results = await data.json()
     consoleLog(results)
 }
@@ -14,7 +14,7 @@ const consoleLog = (data) => {
 
     document.querySelector("body").innerHTML = `
     <main>
-        <div id = "main"></div>    
+        <div id = "main">   
             <h1 id = "location">${data.address}</h1>
             <h1 id = "temp">${data.currentConditions.temp}°</h1>
             <h2 id = "conditions">${data.currentConditions.conditions}</h2>
@@ -25,6 +25,66 @@ const consoleLog = (data) => {
             <h3>Previsión(15 Días)</h3>
             <ul id="lista">
             </ul>
+        </div>
+        
+        <div id = "otherStats">
+            <div id = "feelsLike">
+                <h3>Sensación</h3>
+                <h2>${data.currentConditions.feelslike}°</h2>
+            </div>
+
+            <div id = "uv">
+                <h3>Indice UV</h3>
+                <h2>${data.currentConditions.solarenergy}</h2>
+            </div>
+
+            <div id = "wind">
+                <h3>Viento</h3>
+                <ul>
+                    <li>
+                        <h3>Rachas</h3>
+                        <h3>${data.currentConditions.windgust}km/h</h3>
+                    </li>
+
+                    <li>
+                        <h3>Viento</h3>
+                        <h3>${data.currentConditions.windspeed}km/h</h3>
+                    </li>
+                </ul>
+            </div>
+
+            <div id = "Sunset">
+                <h3>Puesta de Sol</>
+                <h2>${data.currentConditions.sunset}</h2>
+            </div>
+
+            <div>
+                <h3>Precipitación</h3>
+                <h2>${data.currentConditions.precip} mm</h2>
+            </div>
+
+            <div id = "visibility">
+                <h3>Visibilidad</h3>
+                <h2>${data.currentConditions.visibility} km</h2>
+            </div>
+
+            <div id = "Humidity">
+                <h3>Humedad</h3>
+                <h2>${data.currentConditions.humidity} %</h2>
+                <h4>El punto de rocío es de ${data.currentConditions.dew}°</h4>
+            </div>
+
+            <div id = ""pressure>
+                <h3>Presión</h3>
+                <h2>${data.currentConditions.pressure} hPa</h2>
+            </div>
+
+            <div id = "snow">
+                <h3>Nieve</h3>
+                <h2>${data.currentConditions.snow} mm</h2>
+            </div>
+
+
         </div>
     </main>
     `
@@ -43,6 +103,9 @@ const consoleLog = (data) => {
         
         document.querySelector("#lista").appendChild(li)
 }
+
+
+
 
 }
 
--- ProyectoFinal/main.js
+++ ProyectoFinal/main.js
@@ -2,3 +2,5 @@ import './style.css'
 import fetchData from './Componentes/main/main'
 
 fetchData()
+
+
